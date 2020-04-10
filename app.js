0dd06e607e3be28c9b97f7dd713032804218726f
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-04-09 22:44:42 -0500

    fixed cheater bug

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const cells = document.querySelectorAll('.row > div ')
 for (let i = 0; i < cells.length; i++) {
    cells[i].textContent
-   cells[i].addEventListener ('click', cellclicked);
+   cells[i].addEventListener ('click', cellClicked);
 }
 
 let player = '🦄';
 
-function cellclicked(e){
-    e.target.textContent = player;
+function cellClicked(e){
+    if (e.target.textContent === '🦄') {
+    return; 
+   }
+   if (e.target.textContent === '🐲') {
+    return;
+   }
+   e.target.textContent = player;
 
+    togglePlayer();
+}
+
+function togglePlayer(){
     if (player === '🦄') {
         player = '🐲';
     } else {
         player = '🦄';
     }
 }
+  
+   
+ 
 
 
  
